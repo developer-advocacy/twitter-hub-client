811b72a99712cdb3576b174e2b2e7bc704f17763
Josh Long <josh@joshlong.com>
2022-08-21 17:49:05 -0700

    deploy

--- deploy/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -o pipefail
-cd $ROOT_DIR
-./mvnw -DskipTests=true spring-javaformat:apply clean deploy spring-boot:build-image -Dspring-boot.build-image.imageName=$IMAGE_NAME

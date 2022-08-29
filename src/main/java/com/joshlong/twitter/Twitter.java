b981014e2c151e3e89252c83e04cb02f04419c14
Josh Long <josh@joshlong.com>
2022-08-29 01:15:23 -0700

    polish

--- src/main/java/com/joshlong/twitter/Twitter.java
+++ src/main/java/com/joshlong/twitter/Twitter.java
@@ -1,9 +1,13 @@
 package com.joshlong.twitter;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.core.io.Resource;
 import org.springframework.util.Assert;
+import org.springframework.util.Base64Utils;
+import org.springframework.util.FileCopyUtils;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
@@ -27,16 +31,39 @@ public class Twitter {
 
 	private final StreamBridge bridge;
 
-	public Mono<Boolean> scheduleTweet(Client client, Date scheduled, String twitterUsername, String jsonRequest) {
+	private final Client defaultClient;
 
+	public Mono<Boolean> scheduleTweet(Date scheduled, String twitterUsername, String jsonRequest) {
+		Assert.notNull(this.defaultClient, "the defaultClient is null");
+		return this.scheduleTweet(this.defaultClient, scheduled, twitterUsername, jsonRequest);
+	}
+
+	@SneakyThrows
+	private static String base64Encode(Resource media) {
+		if (null != media) {
+			try (var in = media.getInputStream()) {
+				var bytes = FileCopyUtils.copyToByteArray(in);
+				return Base64Utils.encodeToString(bytes);
+			}
+		}
+		return null;
+	}
+
+	public Mono<Boolean> scheduleTweet(Client client, Date scheduled, String twitterUsername, String jsonRequest,
+			Media image) {
 		if (log.isDebugEnabled()) {
-			var map = Map.of("clientSecret: ", client.secret(), "scheduled: ", scheduled, "twitterUsername: ",
-					twitterUsername, "clientId: ", client.id(), "jsonRequest: ", jsonRequest);
+			var map = Map.of("clientSecret:", client.secret(), "scheduled:", scheduled, "twitterUsername:",
+					twitterUsername, "clientId:", client.id(), "jsonRequest:", (Object) jsonRequest);
 			log.debug(map.toString());
 		}
+		var mediaBase64Encoded = (String) null;
+		if (null != image && null != image.resource())
+			mediaBase64Encoded = base64Encode(image.resource());
+
 		var scheduledString = DateUtils.writeIsoDateTime(scheduled);
 		var twitterRequest = new TwitterRequest(client.id(), client.secret(), scheduledString, twitterUsername,
-				jsonRequest);
+				jsonRequest, mediaBase64Encoded);
+
 		log.debug("going to send " + twitterRequest);
 		try {
 			Assert.isTrue(this.bridge.send("twitter-requests", twitterRequest),
@@ -49,11 +76,25 @@ public class Twitter {
 		}
 	}
 
+	public Mono<Boolean> scheduleTweet(Client client, Date scheduled, String twitterUsername, String jsonRequest) {
+		return scheduleTweet(client, scheduled, twitterUsername, jsonRequest, null);
+
+	}
+
+	public record Media(Resource resource, MediaType type) {
+	}
+
+	public enum MediaType {
+
+		IMAGE
+
+	}
+
 	public record Client(String id, String secret) {
 	}
 
 	record TwitterRequest(String clientId, String clientSecret, String scheduled, String twitterUsername,
-			String jsonRequest) {
+			String jsonRequest, String media) {
 	}
 
 	/**
--- src/main/java/com/joshlong/twitter/TwitterServiceClientAutoConfiguration.java
+++ src/main/java/com/joshlong/twitter/TwitterServiceClientAutoConfiguration.java
@@ -2,23 +2,40 @@ package com.joshlong.twitter;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
+import org.springframework.util.StringUtils;
 
 import java.util.Map;
 
 @Configuration
+@EnableConfigurationProperties(TwitterServiceClientProperties.class)
 class TwitterServiceClientAutoConfiguration {
 
 	@Bean
-	Twitter twitterClient(StreamBridge streamBridge) {
-		return new Twitter(streamBridge);
+	Twitter twitterClient(TwitterServiceClientProperties properties, StreamBridge streamBridge) {
+		var client = properties.client();
+		var id = client.id();
+		var secret = client.secret();
+		var defaultClient = (Twitter.Client) null;
+		if (StringUtils.hasText(id) && StringUtils.hasText(secret))
+			defaultClient = new Twitter.Client(id, secret);
+		return new Twitter(streamBridge, defaultClient);
 	}
 
+}
+
+@ConfigurationProperties(prefix = "twitter")
+record TwitterServiceClientProperties(Client client) {
+
+	record Client(String id, String secret) {
+	}
 }
 
 @Slf4j
